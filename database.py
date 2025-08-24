e2ddf06f7b097edc732188530cfbdff2765ae9cc
fairyaisha14 <aishaawoniyi@gmail.com>
2025-08-24 17:01:08 +0100

    database.commit

---
+++ database.py
@@ -0,0 +1,14 @@
+ # Write a program to find what type of datatype the given variable is?
+a = 5
+print ("type of a: ", type(a))
+
+b = 2.5
+print("type of b: ", type(b))
+
+c = "coding"
+print("type of c", type(c))
+
+d = True
+print("type of d:", type(d))
+
+
